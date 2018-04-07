305a6a702f08285ec4c779ff967f86c62b9faf35
Songsterr99 <mr.songsterr@mail.ru>
2018-04-07 10:37:20 +0300

    task7 is modified

--- task7/public/listener.js
+++
@@ -1,9 +0,0 @@
-listener();
-
-function listener(){
-
-  document.getElementById("exit").addEventListener("click", displayDate);
-  function displayDate() {
-      document.getElementById("hi").innerHTML = "<section class="error404"><div class="_404">404</div><h2>Oops, something went wrong</h2><a class="error404-button" href="#">Home page</a></section>";
-  }
-}
